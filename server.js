ddfbfcda56cde758ee00e5484b07b2f5f4c0abc1
Alex Jarvis <alex.jon.jarvis@gmail.com>
2018-08-11 15:07:18 +1000

    fixed directory link

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
